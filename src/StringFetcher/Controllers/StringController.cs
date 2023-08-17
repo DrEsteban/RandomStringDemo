430e445b632b7e71a8f3a2af14a315d94cf92725
Steven Ryland <stevenry@outlook.com>
2023-08-16 17:00:50 -0700

    StringFetcher scaffolding

---
+++ src/StringFetcher/Controllers/StringController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StringFetcher.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StringController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<StringController> _logger;
+
+        public StringController(ILogger<StringController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
